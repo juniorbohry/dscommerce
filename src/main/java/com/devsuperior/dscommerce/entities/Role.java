9c9932c47af9c7a96d0fecd02c488446d6c8aac0
juniorbohry <juniorbohry@gmail.com>
2025-02-15 15:15:05 -0300

    add entity role

---
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -0,0 +1,52 @@
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String Authority;
+
+    public Role() {
+    }
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        Authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return Authority;
+    }
+
+    public void setAuthority(String authority) {
+        Authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+        return Objects.equals(Authority, role.Authority);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(Authority);
+    }
+}
